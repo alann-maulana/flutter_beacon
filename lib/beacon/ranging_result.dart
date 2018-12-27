6813cc2ed3e315109854243c03b91b85aeca6350
Alann Maulana <alann_it06@yahoo.co.id>
2018-12-27 13:33:39 +0700

    Adding dart docs

--- lib/beacon/ranging_result.dart
+++ lib/beacon/ranging_result.dart
@@ -1,15 +1,24 @@
+//  Copyright (c) 2018 Alann Maulana.
+//  Licensed under Apache License v2.0 that can be
+//  found in the LICENSE file.
+
 part of flutter_beacon;
 
+/// Class for managing ranging result from scanning iBeacon process.
 class RangingResult {
+  /// The [Region] of ranging result.
   final Region region;
+
+  /// The [List] of [Beacon] detected of ranging result by [Region].
   final List<Beacon> beacons;
 
+  /// Constructor for deserialize dynamic json into [RangingResult].
   RangingResult.from(dynamic json)
       : region = Region.fromJson(json['region']),
         beacons = Beacon.beaconFromArray(json['beacons']);
 
   @override
-    String toString() {
-      return 'RangingResult{"region": ${json.encode(region.toJson)}, "beacons": ${json.encode(Beacon.beaconArrayToJson(beacons))}}';
-    }
+  String toString() {
+    return 'RangingResult{"region": ${json.encode(region.toJson)}, "beacons": ${json.encode(Beacon.beaconArrayToJson(beacons))}}';
+  }
 }
--- lib/beacon/region.dart
+++ lib/beacon/region.dart
@@ -1,23 +1,45 @@
+//  Copyright (c) 2018 Alann Maulana.
+//  Licensed under Apache License v2.0 that can be
+//  found in the LICENSE file.
+
 part of flutter_beacon;
 
+/// Class for managing ranging region scanning.
 class Region {
+
+  /// The unique identifier of region.
   final String identifier;
+
+  /// The proximity UUID of region.
+  ///
+  /// For Android, this value can be null.
   final String proximityUUID;
+
+  /// The major number of region.
+  ///
+  /// For both Android and iOS, this value can be null.
   final int major;
+
+  /// The minor number of region.
+  ///
+  /// For both Android and iOS, this value can be null.
   final int minor;
 
+  /// Constructor for creating [Region] object.
   Region(
       {@required this.identifier,
       this.proximityUUID,
       this.major,
       this.minor}) : assert(Platform.isIOS && proximityUUID != null);
 
+  /// Constructor for deserialize json [Map] into [Region] object.
   Region.fromJson(dynamic json)
       : identifier = json['identifier'],
         proximityUUID = json['proximityUUID'],
         major = json['major'],
         minor = json['minor'];
 
+  /// Serialize [Region] object into json [Map].
   dynamic get toJson {
     final map = <String, dynamic>{
       'identifier': identifier,

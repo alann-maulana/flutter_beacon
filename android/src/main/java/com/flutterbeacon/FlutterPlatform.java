470d0c828c6a9837338f3be8bccf1af05ba282d8
tristangrobler <dynamitetechdeals@gmail.com>
2023-01-13 16:37:31 +0200

    Added permission

--- android/src/main/java/com/flutterbeacon/FlutterPlatform.java
+++ android/src/main/java/com/flutterbeacon/FlutterPlatform.java
@@ -42,14 +42,11 @@ class FlutterPlatform {
   }
 
   void requestAuthorization() {
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-      ActivityCompat.requestPermissions(getActivity(), new String[]{
-              Manifest.permission.ACCESS_BACKGROUND_LOCATION
-      }, FlutterBeaconPlugin.PERMISSION_REQUEST_BACKGROUND_LOCATION);
-    }
     ActivityCompat.requestPermissions(getActivity(), new String[]{
         Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.ACCESS_FINE_LOCATION
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.BLUETOOTH_SCAN,
+        Manifest.permission.BLUETOOTH_CONNECT,
     }, FlutterBeaconPlugin.REQUEST_CODE_LOCATION);
   }
 
